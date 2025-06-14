377973387520c9d01788fd5c94e041dfbeefe05c
[PoloFighter] <[polofightergfx@gmail.com]>
2025-06-14 21:51:39 +0200

    Github pages absolute path test fix

--- debuginfo.js
+++ debuginfo.js
@@ -1,5 +1,5 @@
 const di = document.querySelector('.debuginfo')
-fetch('debuginfo.html')
+fetch('https://polofighter.github.io/polofighter.com/debuginfo.html')
 .then(res=>res.text())
 .then(data=>{
     di.innerHTML=data
